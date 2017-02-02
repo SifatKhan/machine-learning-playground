f6ab140788104b7721183e717d5d32280811ff48
Javier Matias <javier.l.matias@gmail.com>
2017-02-02 05:44:20 -0400

    Forgot to update the image size in the Flask app

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 import HousePrices.cats_vs_dogs.cvd_model as cvd_model
 
 MODEL_DIR = 'HousePrices/cats_vs_dogs/model/'
-IMAGE_SIZE = 115
+IMAGE_SIZE = 95
 if not exists(MODEL_DIR): makedirs(MODEL_DIR)
 model = cvd_model.CVDModel(img_size=IMAGE_SIZE)
 
