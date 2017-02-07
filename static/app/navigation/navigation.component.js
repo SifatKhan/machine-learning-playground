72606ee975b7a8c856bc72f5280bc0527adb426e
Javier Matias <javier.l.matias@gmail.com>
2017-02-07 00:15:07 -0400

    Wrote to different side navigation columns for Housing Prices and for Classifier

--- static/app/navigation/navigation.component.js
+++ static/app/navigation/navigation.component.js
@@ -1,19 +1,7 @@
-angular.module('myApp').component('sideNavigation', {
-    templateUrl: 'app/navigation/side-navigation.template.html',
-    bindings: {
-      context: '@'
-    },
+angular.module('myApp').component('navigation', {
+    templateUrl: 'app/navigation/navigation.template.html',
     controller: ['$scope',
         function NavigationController($scope) {
-
-        }]
-});
-
-angular.module('myApp').component('topNavigation', {
-    templateUrl: 'app/navigation/top-navigation.template.html',
-    controller: ['$scope',
-        function NavigationController($scope) {
-
         }]
 });
 
