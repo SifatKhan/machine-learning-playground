8163833463ce0ec227093dd748df17bee466c9f3
Javier Matias <javier.l.matias@gmail.com>
2017-02-07 01:25:28 -0400

    Added the convnet graph image.

--- static/app/app.js
+++ static/app/app.js
@@ -12,8 +12,12 @@ angular.module('myApp').config(['$locationProvider', '$routeProvider',
             template: '<pricing-data></pricing-data>'
         }).when('/correlations', {
             template: '<correlations></correlations>'
+        }).when('/about', {
+            template: '<neighborhood-list></neighborhood-list>'
         }).when('/classifier', {
             template: '<image-classifier></image-classifier>'
+        }).when('/aboutclassifier', {
+            templateUrl: 'app/about/aboutclassifier.template.html',
         }).otherwise('/neighborhoods');
     }
 ]);
--- static/app/navigation/navigation.component.js
+++ static/app/navigation/navigation.component.js
@@ -1,7 +1,8 @@
 angular.module('myApp').component('navigation', {
     templateUrl: 'app/navigation/navigation.template.html',
-    controller: ['$scope',
-        function NavigationController($scope) {
+    controller: ['$scope','$location',
+        function NavigationController($scope,$location) {
+            $scope.currentPath = $location.path();
         }]
 });
 
