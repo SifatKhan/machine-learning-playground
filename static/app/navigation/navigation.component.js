00db325a50f8d9718a5a1eff6374c6d8cc6e159e
Javier L. Matias <javier.l.matias@gmail.com>
2017-07-25 21:25:13 -0400

    Removed angular 1 app, going to use the new angular 2 app in https://github.com/jmatias/data-visualization.

--- static/app/navigation/navigation.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp').component('navigation', {
-    templateUrl: 'app/navigation/navigation.template.html',
-    controller: ['$scope','$location',
-        function NavigationController($scope,$location) {
-            $scope.currentPath = $location.path();
-        }]
-});
-
